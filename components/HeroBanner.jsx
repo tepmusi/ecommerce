26534b529e1c0f8eba9d798a7a9f0fbfcf62ed0e
Tep Musi <saving_for_life@hotmail.com>
2022-07-10 21:51:04 +0700

    Added Banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link';
 
-const HeroBanner = () => {
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({heroBanner}) => {
   return (
-    <div>HeroBanner</div>
+    <div className="hero-banner-container">
+      <div>
+        <h3 className="beats-solo">{heroBanner.smallText}</h3>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+      </div>
+
+      <div>
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type="button">{heroBanner.buttonText}</button>
+        </Link>
+        <div className="desc">
+          <h2>Description</h2>
+          <h4>{heroBanner.desc}</h4>
+        </div>
+      </div>
+    </div>
   )
 }
 
